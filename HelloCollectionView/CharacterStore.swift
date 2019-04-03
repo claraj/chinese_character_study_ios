a55a917103db6a071ab19f418efe64d735c07b6e
clara <10088152+claraj@users.noreply.github.com>
2019-04-02 19:20:16 -0500

    fix variable name for fog

--- HelloCollectionView/CharacterStore.swift
+++ HelloCollectionView/CharacterStore.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2019 clara. All rights reserved.
 //
 
-import Foundation
+//import Foundation
 
 class CharacterStore {
     
@@ -14,16 +14,19 @@ class CharacterStore {
     
     init() {
         
-        let snow = Char(chineseCharacter: "雪", pinyin: "Xuě", englishTranslation: "Snow")
-        let wind = Char(chineseCharacter: "风", pinyin: "Fēng", englishTranslation: "Wind")
-        let rain = Char(chineseCharacter: "雨", pinyin: "Yǔ", englishTranslation: "Rain")
-        let warm = Char(chineseCharacter: "暖和", pinyin: "Nuǎnhuo", englishTranslation: "Warm")
-        let sunny = Char(chineseCharacter: "晴朗", pinyin: "Qínglǎng", englishTranslation: "Sunny")
-        let weather = Char(chineseCharacter: "雾", pinyin: "wù", englishTranslation: "Fog")
+        let snow = Char(chineseCharacter: "雪", pinyin: "xuě", englishTranslation: "Snow")
+        let wind = Char(chineseCharacter: "风", pinyin: "fēng", englishTranslation: "Wind")
+        let rain = Char(chineseCharacter: "雨", pinyin: "yǔ", englishTranslation: "Rain")
+        let warm = Char(chineseCharacter: "暖和", pinyin: "nuǎnhuo", englishTranslation: "Warm")
+        let sunny = Char(chineseCharacter: "晴朗", pinyin: "qínglǎng", englishTranslation: "Sunny")
+        let fog = Char(chineseCharacter: "雾", pinyin: "wù", englishTranslation: "Fog")
         
-        chars = [snow, wind, rain, warm, sunny, weather]
+        chars = [snow, wind, rain, warm, sunny, fog]
         chars.shuffle()
         
     }
     
 }
+
+
+
